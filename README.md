ba5e16302c4b141534f58c07f10d628cfe131efa
Erich Reitz <erich.reitz@protonmail.com>
2024-01-26 12:35:43 -0600

    flask/docker structure

---
+++ README.md
@@ -0,0 +1,13 @@
+### Build commands
+
+#### Run locally (outside of docker)
+
+`docker run hs -p 8080:8080`
+
+#### Docker build
+
+`docker run hs -p 8080:8080`
+
+#### Docker run
+
+`docker run hs -p 8080:8080`
---
+++ hs_backend/__init__.py
@@ -0,0 +1,14 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+from hs_backend.config import Config
+
+app = Flask(__name__)   
+app.config.from_object(Config)
+db = SQLAlchemy(app)
+with app.app_context():
+        db.create_all()
+
+
+
+from hs_backend import routes
