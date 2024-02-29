ed7b98bbe0394e47a0514c915b95c07ead2c2e77
Erich Reitz <erich.reitz@protonmail.com>
2024-02-28 21:30:48 -0600

    refactor / remove debug lines from request location permissions screen

---
+++ app/src/main/java/com/ua/historicalsitesapp/ui/components/NoLocationPermissionsScreen.kt
@@ -0,0 +1,54 @@
+package com.ua.historicalsitesapp.ui.components
+
+import android.Manifest
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
+import com.ua.historicalsitesapp.util.hasLocationPermission
+
+
+@Composable
+fun LocationScreen(onPermissionGranted: () -> Unit) {
+    val context = LocalContext.current
+
+    // Create a permission launcher
+    val requestPermissionLauncher =
+        rememberLauncherForActivityResult(
+            contract = ActivityResultContracts.RequestPermission(),
+            onResult = { isGranted: Boolean ->
+                if (isGranted) {
+                    onPermissionGranted()
+                }
+            })
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Button(
+            onClick = {
+                if (!hasLocationPermission(context)) {
+                    requestPermissionLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
+                }
+            }
+        ) {
+            Text(text = "Grant location permissions")
+        }
+        Spacer(modifier = Modifier.height(16.dp))
+    }
+}
--- app/src/main/java/com/ua/historicalsitesapp/ui/screens/MainPageActivity.kt
+++ app/src/main/java/com/ua/historicalsitesapp/ui/screens/MainPageActivity.kt
@@ -1,27 +1,15 @@
 package com.ua.historicalsitesapp.ui.screens
 
-import android.Manifest
-import android.annotation.SuppressLint
-import android.content.Context
-import android.content.pm.PackageManager
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.OnBackPressedCallback
-import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
-import androidx.activity.result.contract.ActivityResultContracts
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
@@ -29,19 +17,17 @@ import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.unit.dp
-import androidx.core.content.ContextCompat
 import androidx.navigation.compose.rememberNavController
 import com.example.compose.HistoricalSitesAppTheme
-import com.google.android.gms.location.LocationServices
 import com.google.android.gms.maps.model.LatLng
 import com.ua.historicalsitesapp.data.model.map.ClusterItem
 import com.ua.historicalsitesapp.nav.AppBottomBar
 import com.ua.historicalsitesapp.nav.BottomNavigationGraph
 import com.ua.historicalsitesapp.ui.components.GoogleMapsScreen
+import com.ua.historicalsitesapp.ui.components.LocationScreen
+import com.ua.historicalsitesapp.util.hasLocationPermission
 import com.ua.historicalsitesapp.viewmodels.MainPageViewModel
 
 class MainPageActivity : ComponentActivity() {
@@ -66,8 +52,7 @@ class MainPageActivity : ComponentActivity() {
 
 }
 
-val TAG = "INFO"
-
+const val TAG = "INFO"
 
 @Composable
 fun MainScreen() {
@@ -82,77 +67,6 @@ fun MainScreen() {
             modifier = Modifier.padding(paddingValues)
         )
     }
-}
-
-
-@Composable
-fun LocationScreen(onPermissionGranted: () -> Unit) {
-    val context = LocalContext.current
-    var location by remember { mutableStateOf("Your location") }
-
-    // Create a permission launcher
-    val requestPermissionLauncher =
-        rememberLauncherForActivityResult(
-            contract = ActivityResultContracts.RequestPermission(),
-            onResult = { isGranted: Boolean ->
-                if (isGranted) {
-                    // Permission granted, update the location
-                    getCurrentLocation(context) { lat, long ->
-                        location = "Latitude: $lat, Longitude: $long"
-                    }
-                    onPermissionGranted()
-                }
-            })
-
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(16.dp),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Button(
-            onClick = {
-                if (hasLocationPermission(context)) {
-                    // Permission already granted, update the location
-                    getCurrentLocation(context) { lat, long ->
-                        location = "Latitude: $lat, Longitude: $long"
-                    }
-                } else {
-                    // Request location permission
-                    requestPermissionLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
-                }
-            }
-        ) {
-            Text(text = "Allow")
-        }
-        Spacer(modifier = Modifier.height(16.dp))
-        Text(text = location)
-    }
-}
-
-private fun hasLocationPermission(context: Context): Boolean {
-    return ContextCompat.checkSelfPermission(
-        context,
-        Manifest.permission.ACCESS_FINE_LOCATION
-    ) == PackageManager.PERMISSION_GRANTED
-}
-
-@SuppressLint("MissingPermission")
-private fun getCurrentLocation(context: Context, callback: (Double, Double) -> Unit) {
-    val fusedLocationClient = LocationServices.getFusedLocationProviderClient(context)
-    fusedLocationClient.lastLocation
-        .addOnSuccessListener { location ->
-            if (location != null) {
-                val lat = location.latitude
-                val long = location.longitude
-                callback(lat, long)
-            }
-        }
-        .addOnFailureListener { exception ->
-            // Handle location retrieval failure
-            exception.printStackTrace()
-        }
 }
 
 
