f69c6e27c8f6c1ac91ea2d777fe5b4d3b2270359
Erich Reitz <erich.reitz@protonmail.com>
2024-04-07 18:54:45 -0500

    make api route a variable in admin website

--- website/src/remnantAPI/GetLocation.tsx
+++ website/src/remnantAPI/GetLocation.tsx
@@ -1,19 +1,18 @@
 import LocationDetails from "../models/Location";
 
+import { API_BASE_URL } from "../ServerUtil";
+
 export const GetLocationDetails = async (
   locationId: string,
   token: string,
   setToken: (token: string) => void,
 ): Promise<LocationDetails> => {
-  const response = await fetch(
-    `http://localhost:8080/api/locations/${locationId}`,
-    {
-      method: "GET",
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
+  const response = await fetch(`${API_BASE_URL}/api/locations/${locationId}`, {
+    method: "GET",
+    headers: {
+      Authorization: `Bearer ${token}`,
     },
-  );
+  });
   if (response.status === 401) {
     setToken("");
     throw new Error("Unauthorized access");
