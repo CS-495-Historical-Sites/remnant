7b828c317f93b56d6d6daa4603ea318eada42c94
Aaron Bouttapanith <144275992+AaronBoutt@users.noreply.github.com>
2024-04-09 22:33:47 -0500

    Username on profile (#54)
    Added username and email to profile page.
    Clicking on username shows a text field with a save button that allows the user to change their username.
    Clicking on the save button makes the editable field disappear.

--- android-app/app/src/main/java/com/ua/historicalsitesapp/data/model/userinfo/UserinfoAddRequest.kt
+++ android-app/app/src/main/java/com/ua/historicalsitesapp/data/model/userinfo/UserinfoAddRequest.kt
@@ -2,4 +2,15 @@ package com.ua.historicalsitesapp.data.model.userinfo
 
 import kotlinx.serialization.Serializable
 
-@Serializable data class UserProfileUpdateRequest(var answers: Set<String>?)
+@Serializable
+data class UserProfileUpdateRequest( val answers: Map<String, Set<String>>?)
+
+@Serializable
+data class UserProfileUsernameRequest( val username: String)
+
+@Serializable
+data class UserProfileInfo(
+    val username: String,
+    val answers: Map<String, Set<String>>?,
+    val email: String
+)
--- android-app/app/src/main/java/com/ua/historicalsitesapp/viewmodels/UserProfileViewModel.kt
+++ android-app/app/src/main/java/com/ua/historicalsitesapp/viewmodels/UserProfileViewModel.kt
@@ -3,40 +3,74 @@ package com.ua.historicalsitesapp.viewmodels
 import android.content.Context
 import androidx.lifecycle.ViewModel
 import com.ua.historicalsitesapp.data.model.auth.LoggedInUser
+import com.ua.historicalsitesapp.data.model.userinfo.UserProfileInfo
 import com.ua.historicalsitesapp.data.model.userinfo.UserProfileUpdateRequest
+import com.ua.historicalsitesapp.data.model.userinfo.UserProfileUsernameRequest
 import com.ua.historicalsitesapp.data.repository.auth.LoginDataSource
 import com.ua.historicalsitesapp.data.repository.auth.LoginRepositoryProvider
 import com.ua.historicalsitesapp.util.ServerConfig
 import com.ua.historicalsitesapp.util.constructUserClient
 import io.ktor.client.HttpClient
-import io.ktor.client.request.post
+import io.ktor.client.call.body
+import io.ktor.client.request.patch
+import io.ktor.client.request.*
 import io.ktor.client.request.setBody
 import io.ktor.http.ContentType
 import io.ktor.http.contentType
 import kotlinx.coroutines.runBlocking
 
+
 class UserProfileViewModel(context: Context) : ViewModel() {
   private val loginRepository =
-      LoginRepositoryProvider.provideLoginRepository(LoginDataSource(), context)
+    LoginRepositoryProvider.provideLoginRepository(LoginDataSource(), context)
 
   private fun getUser(): LoggedInUser {
     return loginRepository.user ?: throw Exception("MainPageViewModel could not retrieve user")
   }
 
-  fun getUserClient(): HttpClient {
+  private fun getUserClient(): HttpClient {
     val user = getUser()
     return constructUserClient(user)
   }
 
   fun submitRegistrationQuestionnaire(answers: Map<String, Set<String>>): Boolean {
     val client = getUserClient()
-    val allAnswers = UserProfileUpdateRequest(answers["Preferred Historical Period:"])
+    val allAnswers = UserProfileUpdateRequest(answers)
     return runBlocking {
       val response =
-          client.post(ServerConfig.SERVER_URL + "api/user/privateinfo") {
-            contentType(ContentType.Application.Json)
-            setBody(allAnswers)
-          }
+        client.patch(ServerConfig.SERVER_URL + "/user/privateinfo") {
+          contentType(ContentType.Application.Json)
+          setBody(allAnswers)
+        }
+      return@runBlocking response.status.value == 200
+    }
+  }
+
+  fun getUsername(username: String): UserProfileInfo {
+    val client = getUserClient()
+    return runBlocking {
+      val response: UserProfileInfo = client.get(ServerConfig.SERVER_URL + "/user/privateinfo" + username) {}.body()
+      return@runBlocking response
+    }
+  }
+
+  fun getEmail(email: String): UserProfileInfo {
+    val client = getUserClient()
+    return runBlocking {
+        val response: UserProfileInfo = client.get(ServerConfig.SERVER_URL + "/user/privateinfo" + email) {}.body()
+        return@runBlocking response
+    }
+  }
+
+  fun updateUsername(username: String): Boolean {
+    val client = getUserClient()
+    val newUsername = UserProfileUsernameRequest(username)
+    return runBlocking {
+      val response =
+        client.patch(ServerConfig.SERVER_URL + "/user/privateinfo") {
+          contentType(ContentType.Application.Json)
+          setBody(newUsername)
+        }
       return@runBlocking response.status.value == 200
     }
   }
