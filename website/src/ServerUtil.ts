2d5905d516e41a2672c83184dcd8df79ed442901
Erich Reitz <erich.reitz@protonmail.com>
2024-04-23 10:23:25 -0500

    fix website server config

--- website/src/ServerUtil.ts
+++ website/src/ServerUtil.ts
@@ -3,4 +3,4 @@ const DEV_BASE_URL = "http://localhost:8080";
 // @ts-ignore
 const PROD_BASE_URL = "https://api.uahistoricalsites.com/";
 
-export const API_BASE_URL = DEV_BASE_URL;
+export const API_BASE_URL = PROD_BASE_URL;
